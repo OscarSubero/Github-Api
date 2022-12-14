3a913ea73df16f792aaa576fbc7ad5c0ca3aa595
OscarSubero <osubero4@gmail.com>
2022-12-14 18:51:26 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// SEMANTIC UI : https://react.semantic-ui.com/usage // npm install semantic-ui-react semantic-ui-css
+// SEMANTIC UI
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
